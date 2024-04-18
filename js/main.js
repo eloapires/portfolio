6b97def9e40494a1e00b30e9e381d902a6f249f8
eloa <165726011+eloapires@users.noreply.github.com>
2024-04-17 21:11:30 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-/*============= NAVBAR ===========*/
+/*================= NAVBAR =================*/
+
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,7 +8,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active')
 }
 
-/*============= ACTIVE LINK =============*/
+/*================= ACTIVE LINK =================*/
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,45 +17,42 @@ window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight; 
+        let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top => offset && top < offset + height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach.apply(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').
-                classList.add('active');
-                });
-            };    
-        });                    
-            
- 
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 
-/*=============== STICKY NAVBAR ===============*/
-let header = document.querySelector('header');
-header.classList.toggle('sticky',window.scrollY > 100);
+ /*================= STICKY NAVBAR =================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
 
-/*=============== REMOVE NAVBAR ===============*/
-menuIcon.classList.remove('faxmark');
+/*================= REMOVE NAVBAR =================*/
+menuIcon.classList.remove('fa-xmark');
 navbar.classList.remove('active');
+};
 
-/*=============== SCROLL REVEAL ===============*/
-
+/*================= SCROLL REVEAL =================*/
 ScrollReveal({
     distance: '80px',
     duration: 2000,
-    delay:200,
+    delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin:'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
-ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
+ScrollReveal().reveal('.home-content, heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, . contact form', {origin:'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin:'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin:'right'});
 
 const typed = new Typed('.multiple-text', {
-    strings:['Desenvolvedor Web', 'Web Designer', 'Youtuber'],
+    strings:['Desenvolvedor Web', 'Web Designer','Youtuber'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
-    loop: true;
+    loop: true
 })
